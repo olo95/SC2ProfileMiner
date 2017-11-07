45c13d981a568b13faca40b5f80e38233355a94b
olo95 <stolar95@onet.pl>
2017-11-07 14:36:51 +0100

    Added necessary classses for build creating functionality

---
+++ SC2ProfileMiner/SC2ProfileMiner/ViewControllers/SC2BuildCell/SC2BuildTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SC2BuildTableViewCell.swift
+//  SC2ProfileMiner
+//
+//  Created by Alexander Stolar on 07.11.2017.
+//  Copyright © 2017 Alexander Stolar. All rights reserved.
+//
+
+import UIKit
+
+class SC2BuildTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ SC2ProfileMiner/SC2ProfileMiner/ViewControllers/SC2BuildPresenter/SC2BuildPresenterViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SC2BuildPresenterViewController.swift
+//  SC2ProfileMiner
+//
+//  Created by Alexander Stolar on 07.11.2017.
+//  Copyright © 2017 Alexander Stolar. All rights reserved.
+//
+
+import UIKit
+
+class SC2BuildPresenterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
